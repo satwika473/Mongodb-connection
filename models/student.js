1856cfaedf145bc617aecb159ffe3cf8bd1da48f
satwika473 <chakkasatwika225@gmail.com>
2025-05-28 15:55:16 +0530

    first

---
+++ models/student.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+const studSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+      course:{
+        type:String,
+        required:true
+    }
+})
+module.exports=mongoose.model('Student',studSchema)
